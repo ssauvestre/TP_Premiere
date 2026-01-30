d9ee2f58c028ef16d85b5fe81d1e683fe8a554f3
ssauvestre <simon.sauvestre@gmail.com>
2026-01-30 08:16:41 +0100

    Add files via upload

---
+++ Influence_Masse.py
@@ -0,0 +1,29 @@
+
+"""
+Activité expérimentale : Influence de la masse sur le vecteur variation de 
+vitesse à l’aide d’un programme Python
+
+"""
+
+""" Ci-dessous, compléter les listes pour la masse marquée M, la vitesse 
+atteinte au bout de 200 ms et 500 ms"""
+M = [] # en g
+v_200ms = [] # en m/s
+v_500ms = [] # en m/s
+
+# Création de trois nouvelles listes
+m_syst = [] # en kg
+dv = [] # en m/s
+dv_sur_dt = [] # en m/s²
+
+""" Compléter les instructions dans la boucle ci-dessous """
+for i in range(0,len(M)):
+    m_syst.append()
+    dv.append()
+    dv_sur_dt.append()
+    
+""" Ci-dessous, compléter le programme pour afficher les différentes masses du 
+système, les valeurs de la variation de vitesse et l'ensemble des valeurs ∆v/∆t
+"""
+    
+    
